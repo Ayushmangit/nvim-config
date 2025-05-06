ca237e5f548657a6e44463a2edf177bffbe7e98b
ayushman <chauhanayushman505@gmail.com>
2025-05-07 00:19:43 +0530

    installed emmit

--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -1,1 +1,8 @@
+return {
     "mattn/emmet-vim",
+    ft = { "html", "css", "javascriptreact", "typescriptreact" }, -- Load only for these filetypes
+    init = function()
+        vim.g.user_emmet_mode = "n"                             -- Normal + Insert mode
+        vim.g.user_emmet_leader_key = "<C-y>"                   -- Trigger key
+    end,
+}
