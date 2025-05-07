3918f72a0d9a54dfd7c633f8fa94e72d30bb52a3
ayushman <chauhanayushman505@gmail.com>
2025-05-07 21:30:35 +0530

    autotag Fix

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -1,9 +1,7 @@
 return {
     "windwp/nvim-ts-autotag",
     event = "VeryLazy",
-    opts = {
-        enable_close = true,
-        enable_rename = true,
-        filetypes = { "html", "jsx", "tsx" }
-    }
+    enable_close = true,
+    enable_rename = true,
+    filetypes = { "html", "jsx", "tsx" }
 }
