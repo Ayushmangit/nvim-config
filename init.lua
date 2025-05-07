c2c355838f603a50391a6e7b8b8d6b2b0c7b80e9
ayushman <chauhanayushman505@gmail.com>
2025-05-07 22:57:06 +0530

    asd

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ require('config.keybinds')
 require('config.lazy')
 
 
-
 -- Format on save using LSP (works with prettierd)
 vim.api.nvim_create_autocmd('BufWritePre', {
     pattern = { '*.js', '*.jsx', '*.ts', '*.tsx', '*.css', '*.html', '*.json', '*.md', '*.yaml' },
